c35a9e5b979cf1ad8050545328474cc8b97128d8
gaoyx <melo@gaoyuexiang.cn>
2017-03-26 22:51:42 +0800

    add MyForm compoment

---
+++ src/myFrom/MyForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+let MyForm = React.createClass({
+  getInitialState: () => {
+    return {content: 'no content submit yet'};
+  },
+  render: () => {
+    return (
+      <div className="MyForm">
+        <div>{this.state.content}</div>
+        <div><input type="text" id="input"/></div>
+        <div><button onclick={this.updateContent}>{this.props.submint | 'submit'}</button></div>
+      </div>
+    );
+  },
+  updateContent: () => {
+    let input = document.getElementById('input');
+    input.value && this.setState({content: input.value});
+    input.value = '';
+  }
+})
